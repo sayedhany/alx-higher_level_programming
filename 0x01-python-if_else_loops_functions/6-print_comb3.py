3e6f0e0392293729931fd5ff1f41a546f3ffc850
sayedhany <sayedh@mail.ru>
2023-05-09 11:53:13 -0700

    solved

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for num1 in range(10):
+    for num2 in range(10):
+        if num1 == 9 and num2 == 9:
+            print("{}{}".format(num1, num2))
+            break
+        if num1 == num2 or num1 > num2:
+            continue
+        if num1 != 9:
+            print("{}{}, ".format(num1, num2), end="")
