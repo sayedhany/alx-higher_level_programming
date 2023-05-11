ca3369ca48f68a226a9c96a4f2df391e4f33872f
sayedhany <sayedh@mail.ru>
2023-05-11 11:53:55 -0700

    solved

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add as add1
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add1(a, b)))
