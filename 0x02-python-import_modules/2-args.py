d78375b02979fa7786733db2b9ca5ccd34f49cae
sayedhany <sayedh@mail.ru>
2023-05-11 13:08:35 -0700

    solved

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,12 @@
 import sys
 if __name__ == "__main__":
     arrlen = len(sys.argv)
+
     if arrlen == 1:
-        print("{:d} argument".format(arrlen))
+        print("{:d} arguments.".format(arrlen - 1))
+    elif arrlen == 2:
+        print("{:d} argument:".format(arrlen - 1))
     else:
-        print("{:d} arguments".format(arrlen))
-    for count in range(1, arrlen):
-        print("{:d}: {:s}".format(count, sys.argv[count]))
+        print("{:d} arguments:".format(arrlen - 1))
+        for count in range(1, arrlen):
+            print("{:d}: {:s}".format(count, sys.argv[count]))
