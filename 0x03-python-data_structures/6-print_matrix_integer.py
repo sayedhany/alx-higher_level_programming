e1e628544102c880f2170261eda2a0b9aab2886e
sayedhany <sayedh@mail.ru>
2023-05-14 12:25:23 -0700

    solved

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for max1 in matrix:
-        for element in max1:
-            print("{:d} ".format(element), end="")
-        print()
+        if len(matrix) == 0:
+            print()
+        for n in range(len(max1)):
+            print('{:d}'.format(max1[n]),
+                  end='\n' if n == len(max1) - 1 else ' ')
+
