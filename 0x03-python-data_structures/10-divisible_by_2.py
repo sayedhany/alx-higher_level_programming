4692cfdc152eab8fe50952afeaeda871c74d9404
sayedhany <sayedh@mail.ru>
2023-05-15 06:12:28 -0700

    edded

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    newlist = []
+    for num in my_list:
+        if num % 2 == 0:
+            newlist.push(True)
+        else:
+            newlist.push(False)
+    return newlist
