3098e7d87fd574ace7bb8d6d7c7349e5e0c4e6b0
sayedhany <sayedh@mail.ru>
2023-05-29 08:01:05 -0700

    solved

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for num in my_list:
+    for idx in range(x):
         try:
-            if (isinstance(num, int) and count < x):
-                print("{:d}".format(num), end="")
-                count += 1
+            print("{:d}".format(my_list[idx]), end="")
+            count += 1
         except (TypeError, ValueError):
             pass
     print()
