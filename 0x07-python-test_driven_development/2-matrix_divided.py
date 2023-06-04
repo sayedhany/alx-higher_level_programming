7beab3dfa34fccf299916d7207b84e9a2a048dcb
sayedhany <sayedh@mail.ru>
2023-06-04 08:45:01 -0700

    solved

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,6 +2,8 @@
 """
 divide matix by div
 """
+
+
 def matrix_divided(matrix, div):
     """
     divide matix with div
@@ -11,11 +13,11 @@ def matrix_divided(matrix, div):
         check if matrix is list of lists
         """
         bool_list = isinstance(variable, list)
-        bool_lists = all(isinstance(item, list) for item in variable) 
+        bool_lists = all(isinstance(item, list) for item in variable)
         return bool_list and bool_lists
-
+    mess_err = "matrix must be a matrix (list of lists) of integers/floats"
     if not is_list_of_lists(matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(mess_err)
     len_row = len(matrix[0])
     for row in matrix:
         if len_row != len(row):
@@ -25,7 +27,7 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     divided_matrix = [
             [round(elem / div, 2) for elem in row]
             for row in matrix
