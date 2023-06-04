8014d60dd0fc90cc4f2e76043d76e0baa1e93dcc
sayedhany <sayedh@mail.ru>
2023-06-04 10:58:47 -0700

    added

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,8 +6,12 @@ def say_my_name(first_name, last_name=""):
     """
     print full name
     """
-    if type(first_name) not in [str]:
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if type(last_name) not in [str]:
-            raise TypeError("last_name must be a string")
+    
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("./tests/3-say_my_name.txt")
