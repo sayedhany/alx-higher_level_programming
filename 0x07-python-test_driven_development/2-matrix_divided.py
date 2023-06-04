1f362b9ed77278fe251d7bf2366b89b950beb36a
sayedhany <sayedh@mail.ru>
2023-06-04 09:10:28 -0700

    solved

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,7 +14,13 @@ def matrix_divided(matrix, div):
         """
         bool_list = isinstance(variable, list)
         bool_lists = all(isinstance(item, list) for item in variable)
-        return bool_list and bool_lists
+        check_int_float = True
+        for row in variable:
+            for el in row:
+                if type(el) not in [int, float]:
+                    check_int_float = False
+                    break
+        return bool_list and bool_lists and check_int_float
     mess_err = "matrix must be a matrix (list of lists) of integers/floats"
     if not is_list_of_lists(matrix):
         raise TypeError(mess_err)
@@ -37,4 +43,4 @@ def matrix_divided(matrix, div):
 
 if __name__ == "__main__":
     import doctest
-    doctest.testfile("2-matrix_divided.txt")
+    doctest.testfile("tests/2-matrix_divided.txt")
