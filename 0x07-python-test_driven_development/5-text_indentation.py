d855737b8b67a686e5f9244abadec3537180d614
sayedhany <sayedh@mail.ru>
2023-06-04 11:33:05 -0700

    solved

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+prints a text with 2 new lines after each of these characters: ., ? and :
+"""
+def text_indentation(text):
+    """
+    print new line after ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+                [line.strip(" ") for line in text.split(delim)])
+    print("{}".format(text), end="")
