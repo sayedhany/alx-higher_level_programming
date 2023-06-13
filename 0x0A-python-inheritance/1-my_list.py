5ed37208e16cb11ea7ac2d1d85be36519f8f6dd9
sayedhany <sayedh@mail.ru>
2023-06-12 17:17:20 -0700

    solveD

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,12 +5,12 @@ print sorted
 
 
 class MyList(list):
+    """class"""
+    pass
+
     """
     function to print sorted
     """
     def print_sorted(self):
         sorted_lisst = sorted(self)
         print(sorted_lisst)
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/1-my_list.txt")
