bce3a725165e29d0643ad8bc1d7b45e15a8cd7ad
sayedhany <sayedh@mail.ru>
2023-06-13 15:23:03 -0700

    solveD

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class to JSON"""
+
+
+def class_to_json(obj):
+    new_dict = {}
+
+    for attr in dir(obj):
+        attr_value = getattr(obj, attr_name)
+
+        if isinstance(attr_value, (list, dict, str, int, bool)):
+            new_dict[attr] = attr_value
+    return new_attr
